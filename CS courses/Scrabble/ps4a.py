b8cfc5d76c4ba3a1a38e804c0e42fd6148a7a79d
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-07-26 09:48:30 -0700

    created helper function calculateHandlen() to calculate number of letters in hand

--- CS courses/Scrabble/ps4a.py
+++ CS courses/Scrabble/ps4a.py
@@ -209,8 +209,12 @@ def calculateHandlen(hand):
     hand: dictionary (string-> int)
     returns: integer
     """
-    # TO DO... <-- Remove this comment when you code this function
 
+    length = 0
+    for letter in hand:
+        length += hand[letter]
+
+    return length
 
 
 def playHand(hand, wordList, n):
