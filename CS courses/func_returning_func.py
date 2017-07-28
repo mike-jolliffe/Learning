dbd8169b2df90f277a258bee73f7746dfb90dbbe
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-07-28 16:06:04 -0700

    finished function that returns function

--- CS courses/func_returning_func.py
+++ CS courses/func_returning_func.py
@@ -3,9 +3,21 @@ def general_poly (L):
     Returns a function, which when applied to a value x, returns the value
     n0 * x^k + n1 * x^(k-1) + ... nk * x^0 """
 
-    def poly_part(x, n, k):
-        """ given a set of ints x, n, k, returns n * x ^ k"""
-        return n * x ** k
+    def poly_part(x):
+        """ calculates the polynomial value given coefficient x"""
+        # create a sum variable
+        total = 0
 
+        # initialize k, which is the total number of elements minus 1 (want to end on zero)
+        k = len(L) - 1
+
+        # for every coefficient element, n, in the list
+        for n in L:
+            # add its value to the total
+            total += (n * x ** k)
+            k -= 1
+        return total
 
     return poly_part
+
+print(general_poly([1, 2, 3, 4])(10))
