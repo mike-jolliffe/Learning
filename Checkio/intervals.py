c951cc3e2cb56801b4760e12755fa34a6df9e448
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-07-30 10:45:33 -0700

    pseudocode for function that breaks set into min/max tuples of even-incremented intervals

---
+++ Checkio/intervals.py
@@ -0,0 +1,13 @@
+def intervals(nums):
+  '''Given a set of ints, breaks into tuples covering all different intervals
+     occurring across the nums.'''
+
+  # initialize a list to track an interval
+
+  # for a given pair in nums, if interval between them > 1
+    # create a break-point between intervals
+    # break the loop
+
+  # otherwise, append that item into an interval
+
+  # for the interval, sort ascending and convert to a tuple of max, min 
