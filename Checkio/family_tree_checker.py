31996882d089f4d6b80a1e81d0255153059f7658
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-07-31 16:49:30 -0700

    updated checker

--- Checkio/family_tree_checker.py
+++ Checkio/family_tree_checker.py
@@ -2,10 +2,27 @@ def is_family(data):
   '''Given a list of parent-child element sublists, returns a boolean of whether
   a family tree is correct'''
 
-  # initialize a dictionary, where parent is key, child is value
+  # initialize a family tree, where person is key, heirarchy is value
   tree = {}
 
+
   # for each parent-child sublist
-    # if parent is a key already, add child as value to that key
-    # or if parent is a value, replace that value with a key-value sub-dictionary
-    # or if child is a key, create new dict key above everything for parent
+  for par_chil in data:
+      # if parent in dictionary
+      if not par_chil[0] in tree:
+          # increment by one
+          tree[par_chil[0]] = 0
+
+      # if child in dictionary
+      if par_chil[1] in tree:
+          # add its parent score, and increment by one
+          tree[par_chil[1]] += (tree[par_chil[0]]+1)
+      else:
+          tree[par_chil[1]] = (tree[par_chil[0]]+1)
+
+  for par_chil in data:
+      if tree[par_chil[0]] > tree[par_chil[1]]:
+          return False
+  return True
+
+print(is_family([["Logan","Mike"],["Logan","Jack"]]))
