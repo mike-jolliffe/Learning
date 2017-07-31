874df3b8413b38513fd79aedbeb811cd82e815dc
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-07-31 13:49:40 -0700

    created class intSet that allows operations on a set of ints

---
+++ CS courses/intset_class.py
@@ -0,0 +1,43 @@
+class intSet(object):
+    """An intSet is a set of integers
+    The value is represented by a list of ints, self.vals.
+    Each int in the set occurs in self.vals exactly once."""
+
+    def __init__(self):
+        """Create an empty set of integers"""
+        self.vals = []
+
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self"""
+        if not e in self.vals:
+            self.vals.append(e)
+
+    def member(self, e):
+        """Assumes e is an integer
+           Returns True if e is in self, and False otherwise"""
+        return e in self.vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self
+           Raises ValueError if e is not in self"""
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+
+    def intersect(self, other):
+        '''Returns a new intSet of integers appearing in both self and other.
+        Returns empty set if no intersection'''
+        intersect = set()
+
+        for val in self.vals:
+            if val in other.vals:
+                intersect.add(val)
+        if len(intersect) == 0:
+            return {}
+        return intersect
+
+    def __str__(self):
+        """Returns a string representation of self"""
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
