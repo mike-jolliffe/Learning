65ec0735cc88b1e0b3b04daeea6378f3da08f11a
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-07-31 18:02:17 -0700

    checker finished

--- Checkio/family_tree_checker.py
+++ Checkio/family_tree_checker.py
@@ -4,25 +4,63 @@ def is_family(data):
 
   # initialize a family tree, where person is key, heirarchy is value
   tree = {}
-
+  par_val = 0
+  chil_val = 0
 
   # for each parent-child sublist
   for par_chil in data:
-      # if parent in dictionary
-      if not par_chil[0] in tree:
-          # increment by one
+      # if parent exists in dictionary
+      if par_chil[0] in tree:
+          # get that parent value and set to par_val
+          par_val = tree[par_chil[0]]
+      # otherwise
+      else:
+          # otherwise, insert into dictionary and set parent value to zero
           tree[par_chil[0]] = 0
-
+          par_val = 0
       # if child in dictionary
       if par_chil[1] in tree:
-          # add its parent score, and increment by one
-          tree[par_chil[1]] += (tree[par_chil[0]]+1)
+        # get its value and bind to chil_val
+        chil_val = tree[par_chil[1]]
       else:
-          tree[par_chil[1]] = (tree[par_chil[0]]+1)
+          # update chil_val and increment by (parent + one)
+          chil_val = par_val + 1
+          tree[par_chil[1]] = chil_val
 
-  for par_chil in data:
-      if tree[par_chil[0]] > tree[par_chil[1]]:
+      # if parent scores >= to child, or Mike not chile to anyone, not Mikes family tree
+      if par_val >= chil_val:
           return False
+  if tree['Mike'] == 0:
+      return False
   return True
 
-print(is_family([["Logan","Mike"],["Logan","Jack"]]))
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert is_family([
+      ['Logan', 'Mike']
+    ]) == True, 'One father, one son'
+    assert is_family([
+      ['Logan', 'Mike'],
+      ['Logan', 'Jack']
+    ]) == True, 'Two sons'
+    assert is_family([
+      ['Logan', 'Mike'],
+      ['Logan', 'Jack'],
+      ['Mike', 'Alexander']
+    ]) == True, 'Grandfather'
+    assert is_family([
+      ['Logan', 'Mike'],
+      ['Logan', 'Jack'],
+      ['Mike', 'Logan']
+    ]) == False, 'Can you be a father for your father?'
+    assert is_family([
+      ['Logan', 'Mike'],
+      ['Logan', 'Jack'],
+      ['Mike', 'Jack']
+    ]) == False, 'Can you be a father for your brather?'
+    assert is_family([
+      ['Logan', 'William'],
+      ['Logan', 'Jack'],
+      ['Mike', 'Alexander']
+    ]) == False, 'Looks like Mike is stranger in Logan\'s family'
+    
