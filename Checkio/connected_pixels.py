7f0bdac2719ae95c858e3ef4425088c58fe8f1c5
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-04 15:48:29 -0700

    list all connections between pairs of pixels

--- Checkio/connected_pixels.py
+++ Checkio/connected_pixels.py
@@ -8,13 +8,28 @@ def connected_pixels(list_of_ij):
   for i in range(len(list_of_ij)):
       for j in range(len(list_of_ij[i])):
           if list_of_ij[i][j] == 1:
-              ones.append([i,j])
-  print(ones)
+              ones.append((i,j))
+
   # create a holder for storing connected desireds
   connections = []
-  # for each of the desired pixels, compare others to it to see if connected
+  # for each of the desired pixels, grab its connections
+  for i in range(len(ones)-1):
+      for j in range(i+1, len(ones)):
+          if abs(ones[i][0]-ones[j][0]) < 2 and abs(ones[i][1] - ones[j][1]) < 2:
+              connections.append((ones[i], ones[j]))
+          else:
+              connections.extend([(ones[i]), (ones[j])])
+  print (connections)
 
-  # store the connected
+  # TODO fix the following code so it returns unioned sets or stand-alones
+  final = []
+  # create unions from the individual connections so not double counting
+  for i in range(len(connections)):
+      for j in range(i+1, len(connections)):
+          if connections[j][0] in connections[i] or connections[j][1] in connections[i]:
+              final.append(list(set(connections[j]) | set(connections[i])))
+
+  print (final)
 
 connected_pixels([
     [0, 0, 0, 0, 0],
