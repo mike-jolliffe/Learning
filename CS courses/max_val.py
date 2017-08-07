766c588e6b668cd97f870ecaf6fbb365a39bf143
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-06 18:33:49 -0700

    WIP

--- CS courses/max_val.py
+++ CS courses/max_val.py
@@ -4,17 +4,18 @@ def max_val(t):
         No tuple or list is empty
         Returns the maximum int in t or (recursively) in an element of t """
 
-    # if this is the final element, return it
-    if len(t) == 1:
-        return t[0]
+    if t == [] or t == ():
+        return t
 
-    # otherwise, call max_val on it, returning the greater value
+    elif type(t) == int:
+        return t
+
+    # if this is the final element, return it
+    # TODO figure out how to recursively call this
     else:
-        m = max_val(t[1:])
-        if type(t[0]) == int:
-            return m if m > t[0] else t[0]
-        else:
-            return max_val(t[0])
+        m = max_val(t[0])
+        return m
+
 
 print(max_val((1, 2, [5, 6, (1, 2, 9), 7], 2, 3)))
 print (max_val((9, [3, 8, 2])))
