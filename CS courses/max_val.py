987674dd849b166b2f59c71f9011848ce0750b34
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-06 17:36:51 -0700

    recursive to return max across all list elements, including lists of lists of lists, etc.

--- CS courses/max_val.py
+++ CS courses/max_val.py
@@ -3,18 +3,18 @@ def max_val(t):
         Each element of t is either an int, a tuple, or a list
         No tuple or list is empty
         Returns the maximum int in t or (recursively) in an element of t """
-    # convert everything to same type, a list
-    if type(t) == tuple:
-        t = list(t)
-    for elmnt in t:
-        if type(elmnt) == tuple:
-            elmnt = list(elmnt)
 
-    if t == [] or t == ():
-        return list(t)
+    # if this is the final element, return it
+    if len(t) == 1:
+        return t[0]
 
-    if type(t[0]) == int:
-        return t[:1] + max_val(list(t[1:]))
-    return max_val(t[0]) + max_val(list(t[1:]))
+    # otherwise, call max_val on it, returning the greater value
+    else:
+        m = max_val(t[1:])
+        if type(t[0]) == int:
+            return m if m > t[0] else t[0]
+        else:
+            return max_val(t[0])
 
 print(max_val((1, 2, [5, 6, (1, 2, 9), 7], 2, 3)))
+print (max_val((9, [3, 8, 2])))
