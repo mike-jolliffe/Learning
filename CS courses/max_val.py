2e26a859e5da6baad6ca7fc86b56ebbf458653d9
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-07 08:24:28 -0700

    split up flattening and grabbing max into two functions -- helper to flatten, then simple function that calls it then grabs max of the flattened list

--- CS courses/max_val.py
+++ CS courses/max_val.py
@@ -4,18 +4,24 @@ def max_val(t):
         No tuple or list is empty
         Returns the maximum int in t or (recursively) in an element of t """
 
-    if t == [] or t == ():
-        return t
+    def flatten(t):
+        '''Helper function that takes t and flattens it into a list of ints'''
+        if t == [] or t == () or type(t) == int:
+            return [t]
 
-    elif type(t) == int:
-        return t
+        elif len(t) == 1 and type(t[0]) == int:
+            return t[:1]
 
-    # if this is the final element, return it
-    # TODO figure out how to recursively call this
-    else:
-        m = max_val(t[0])
-        return m
+        elif len(t) == 1 and (type(t[0]) == list or type(t[0]) == tuple):
+            return flatten(t[0])
 
+        # if this is the final element, return it
+        # TODO figure out how to recursively call this
+        else:
+            return flatten(t[0]) + flatten(list(t[1:]))
+
+    lst = flatten(t)
+    return max(lst)
 
 print(max_val((1, 2, [5, 6, (1, 2, 9), 7], 2, 3)))
 print (max_val((9, [3, 8, 2])))
