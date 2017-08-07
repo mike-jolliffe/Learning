f5c194f3923a89c3268f50f298a578c07b5be901
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-07 09:42:21 -0700

    modified to catch corner case of returning tuple instead of list by returning list(t[:1]). Debugged and unit tested

--- CS courses/max_val.py
+++ CS courses/max_val.py
@@ -10,7 +10,7 @@ def max_val(t):
             return [t]
 
         elif len(t) == 1 and type(t[0]) == int:
-            return t[:1]
+            return list(t[:1])
 
         elif len(t) == 1 and (type(t[0]) == list or type(t[0]) == tuple):
             return flatten(t[0])
@@ -25,3 +25,4 @@ def max_val(t):
 
 print(max_val((1, 2, [5, 6, (1, 2, 9), 7], 2, 3)))
 print (max_val((9, [3, 8, 2])))
+print(max_val(((4,),(2,),(6,2),(3,6,2))))
