c3b45d12eda3d4852a85441295ac671000a116ab
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-08 16:35:37 -0700

    create public class Dog{} that assigns attributes to several variables, such as name, weight, friendliness

---
+++ CS_courses/IntroToJava/src/problems/Dog1.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class Dog1 {
+
+	public static void main(String[] args) {
+		// declare correct variable type for dog attributes
+		String name = "Buster";
+		float weight = 25.5F;
+		float tail_length = 2.2F;
+		boolean friendly = false;
+
+		System.out.println(name + " weighs " + weight + " pounds");
+		System.out.println(name + "'s tail is " + tail_length + " inches long");
+		System.out.println("Is " + name + " friendly?  " + friendly);
+	}
+
+}
