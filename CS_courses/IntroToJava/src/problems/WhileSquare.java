e9ad15c4c99c98b8507f74593c1787ef68204446
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-09 15:36:36 -0700

    while loop squaring each element

---
+++ CS_courses/IntroToJava/src/problems/WhileSquare.java
@@ -0,0 +1,16 @@
+package problems;
+
+public class WhileSquare {
+
+	public static void main(String[] args) {
+		// use while loop to compute the square of each element
+		int i = 0;
+		while (i < 9) {
+			int sqrd = i * i;
+			System.out.println(i + " squared is " + sqrd);
+			i++;
+		}
+
+	}
+
+}
