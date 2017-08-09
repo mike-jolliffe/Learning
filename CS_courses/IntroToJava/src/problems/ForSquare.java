8e01ccd0584f11750b50cd25f5d5fb0ad9a55b88
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-09 15:32:45 -0700

    for loop squaring each element

---
+++ CS_courses/IntroToJava/src/problems/ForSquare.java
@@ -0,0 +1,16 @@
+package problems;
+
+public class ForSquare {
+
+	public static void main(String[] args) {
+		// moving through for loop, squares each element
+		for (int i = 0; i < 9; i++) {
+			int num = i;
+			int sqrd = num * num;
+			
+			System.out.println(num + " squared is " + sqrd);
+		}
+
+	}
+
+}
