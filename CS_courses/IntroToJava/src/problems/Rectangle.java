96f9f460a95522decb2a8fc6a6ab01d9699bfc2d
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-15 16:04:51 -0700

    created subclass Rectangle() that extends Shape(), and implemented getArea() method specific to rectangle objects

---
+++ CS_courses/IntroToJava/src/problems/Rectangle.java
@@ -0,0 +1,45 @@
+package problems;
+
+public class Rectangle extends Shape {
+	// Rectangle fields
+	private float width;
+	private float height;
+
+	// Rectangle constructors
+	public Rectangle() {
+		this(0, 0, Color.GREEN, 1, 1);
+		
+	}
+
+	// Getters and setters for height and width
+	public Rectangle(float x, float y, Color shapeColor, float width, float height) {
+		super(0,0,Color.GREEN);
+		this.width = width;
+		this.height = height;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	// Implementation of getArea()
+	@Override
+	public float getArea() {
+		return this.width * this.height; 
+		
+	}
+
+	
+}
