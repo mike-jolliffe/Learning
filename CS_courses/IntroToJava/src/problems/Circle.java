5f94d2075ab087e87c04013ba3d99361ca921004
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-15 16:05:44 -0700

    created Circle() subclass with its own getArea() method

---
+++ CS_courses/IntroToJava/src/problems/Circle.java
@@ -0,0 +1,34 @@
+package problems;
+
+public class Circle extends Shape {
+	// circle radius field
+	float radius;
+	
+	// Constructors
+	public Circle() {
+		this(0,0,Color.GREEN, 1);
+	}
+
+	public Circle(int x, int y, Color shapeColor, float radius) {
+		super(x, y, shapeColor);
+		this.radius = radius;
+	}
+
+	// Getters and Setters
+	public float getRadius() {
+		return radius;
+	}
+
+	public void setRadius(float radius) {
+		this.radius = radius;
+	}
+
+	// Implementation of getArea() method
+	@Override
+	public float getArea() {
+		return (float)(Math.PI * Math.pow(radius, 2.0F));
+		
+	}
+	
+	
+}
