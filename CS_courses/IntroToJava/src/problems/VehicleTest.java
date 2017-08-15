d6f2cf13c89d4fddefc74e38155b810d815f1b9e
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-15 13:00:01 -0700

    modified vehicle test to loop through an array of Vehicle() objects, printing each. Testing Liskov's Substitution Principle on the array. Subclass can substitute for superclass, but not other direction

--- CS_courses/IntroToJava/src/problems/VehicleTest.java
+++ CS_courses/IntroToJava/src/problems/VehicleTest.java
@@ -8,8 +8,18 @@ public class VehicleTest {
 		Vehicle atv = new Vehicle(45, 3);
 		Bicycle bike = new Bicycle(30, 10);
 		
-		System.out.println(atv);
-		System.out.println(bike);
+		//System.out.println(atv);
+		//System.out.println(bike);
+		
+		Vehicle[] vehicles = new Vehicle[3];
+		
+		vehicles[0] = new Bicycle(30, 10);
+		vehicles[1] = new Bicycle(20, 3);
+		vehicles[2] = new Vehicle();
+		
+		for (Vehicle vehicle : vehicles) {
+			System.out.println(vehicle);
+		}
 	}
 
 }
