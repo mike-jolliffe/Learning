b8f6559ed155c0a5ce3e2f86c56d2485030f72d6
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-15 15:30:00 -0700

    created abstract class Shape() for use by subclasses

---
+++ CS_courses/IntroToJava/src/problems/Shape.java
@@ -0,0 +1,52 @@
+package problems;
+
+public abstract class Shape {
+	
+	// fields for x, y location and color
+	private int x;
+	private int y;
+	private Color shapeColor;
+	
+	// constructors
+	public Shape() {
+		this(0, 0, Color.GREEN);
+	}
+
+	public Shape(int x, int y, Color shapeColor) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.shapeColor = shapeColor;
+	}
+
+	// getters and setters
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Color getShapeColor() {
+		return shapeColor;
+	}
+
+	public void setShapeColor(Color shapeColor) {
+		this.shapeColor = shapeColor;
+	}
+	
+	// methods
+	public abstract void getArea();
+	
+	
+	
+}
