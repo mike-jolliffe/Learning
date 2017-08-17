0211cecdb02449dd06447357a6df1c476c7ce6e1
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-17 11:56:35 -0700

    working with Try/Catch blocks for dealing with Exception objects

---
+++ CS_courses/IntroToJava/src/problems/ParseTest.java
@@ -0,0 +1,28 @@
+package problems;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class ParseTest {
+	public static void main(String[] args) {
+		NumberFormat format = NumberFormat.getCurrencyInstance();
+		String s;
+		Number num;
+		s = "$45.67";
+		//s = "hi mom"; //uncomment this for an error
+
+		try {
+			num = format.parse(s); // may generate exception
+			System.out.println("Float value = " + num.floatValue());
+		}
+
+		catch (ParseException e) {
+			System.err.println("Invalid string \"" + s + "\"");
+		}
+
+		catch (Exception e) {
+			System.out.println("Invalid string \"" + s + "\"");
+		}
+
+	}
+}
