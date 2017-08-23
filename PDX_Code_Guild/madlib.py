a0655228f0a18bffda27a57b41aa1f1b5a8076e7
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-23 16:00:46 -0700

    made function more modular for use eleswhere

--- PDX_Code_Guild/madlib.py
+++ PDX_Code_Guild/madlib.py
@@ -1,15 +1,21 @@
 import random
 
+def madlib_builder(user_input):
+    '''Prints a madlib that formats a string to include user-inputted adjectives'''
+
+    # Split user input into an array of three elements
+    adj_array = user_input.split(", ")
+    # Using random.shuffle(), insert the three adjectives into the story
+    random.shuffle(adj_array)
+    return adj_array
+
+def madlib_printer(array):
+    # Print the result as a silly formatted string back to the console
+    print("At the zoo, I saw a {} giraffe, a {} penguin, and a {} zebra".format(array[0],
+        array[1], array[2]))
+
 # Get user input
 user_input = input("Give me three comma-separated adjectives: ")
 
-def madlib_builder(user_input):
-    # Split user input into an array of three elements
-    adj_array = user_input.split()
-    # Using random.shuffle(), insert the three adjectives into the story
-    random.shuffle(adj_array)
-    # Print the result as a silly formatted string back to the console
-    print("At the zoo, I saw a {} giraffe, a {} penguin, and a {} zebra".format(adj_array[0],
-        adj_array[1], adj_array[2]))
-# Call the function on the user's input, so when file is executed, function is called
-madlib_builder(user_input)
+# Call the function on the user's input
+madlib_printer(madlib_builder(user_input))
