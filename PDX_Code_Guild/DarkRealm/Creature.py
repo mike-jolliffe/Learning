9bfe1ab04a7907321cd5de4b7794b0e1ea8c3f15
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-09-05 16:31:45 -0700

    modified move() method to limit movement to within the walls of a room

--- PDX_Code_Guild/DarkRealm/Creature.py
+++ PDX_Code_Guild/DarkRealm/Creature.py
@@ -1,3 +1,5 @@
+from Room import *
+
 class Creature(object):
     '''Used for creating a creature, giving it stats, and making it fight'''
 
@@ -6,12 +8,15 @@ class Creature(object):
         self.weapon = weapon
         self.location = location
 
-    def move(self, dir):
+    def move(self, dir, Room):
         '''Given a direction tuple, updates creature object's location on the board'''
-        # TODO resolve directions so NSEW actually move player NSEW
         move_dict = {"N": (1,0), "S": (-1, 0), "E": (0, 1), "W": (0, -1)}
+        location_check = []
         if dir in move_dict:
-            self.location = tuple(x + y for x, y in zip(self.location, move_dict[dir]))
+            #TODO resolve movements that would put player through door
+            location_check = tuple(x + y for x, y in zip(self.location, move_dict[dir]))
+            if location_check[0] in range(Room.size[0]) and location_check[1] in range(Room.size[1]):
+                self.location = tuple(x + y for x, y in zip(self.location, move_dict[dir]))
         return self.location
 
     def attack(self):
