bb74b3d632f9441955db845e44c33b24490d0dc9
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-09-05 16:12:24 -0700

    created Play() file for playing DarkRealm game

---
+++ PDX_Code_Guild/DarkRealm/Play.py
@@ -0,0 +1,15 @@
+from Creature import *
+from Item import *
+from Room import *
+
+if __name__ == '__main__':
+    room1 = Room((7, 3), "easy")
+    mike = Hero(100, "Sword", room1.entrance, "Mithril", {})
+    while True:
+        room1.build_Room(mike)
+        room1.display_Room()
+        dir = input("Which direction would you like to move? ")
+        if dir == "Q":
+            exit()
+        mike.move(dir)
+        print(mike.location)
