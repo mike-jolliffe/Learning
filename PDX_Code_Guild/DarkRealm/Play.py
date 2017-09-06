1ede1f520584bfb673fc8f0427885dd1a12f8897
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-09-06 15:44:43 -0700

    added a health/armor/inventory look-up

--- PDX_Code_Guild/DarkRealm/Play.py
+++ PDX_Code_Guild/DarkRealm/Play.py
@@ -17,8 +17,12 @@ if __name__ == '__main__':
     print()
 
     name = input(("PICK A NAME FOR YOUR HERO: "))
+
+    print(f"Welcome, {name}! Steel thy nerves for you are about to encounter some terrifying monsters!!!")
+
+
     while True:
-        room1 = Room((random.randint(4,20), random.randint(3,20)), 2)
+        room1 = Room((random.randint(4,15), random.randint(3,15)), 2)
         hero = Hero(100, "Sword", room1.entrance, "Mithril", {})
         baddies = room1.seed_Creatures()
         while True:
@@ -27,6 +31,12 @@ if __name__ == '__main__':
             dir = input(f"Which direction would you like {name} to move? (n)orth, (s)outh, (e)ast, or (w)est? ")
             if dir == "Q":
                 exit()
+            elif dir == "1":
+                print(f"Health: {hero.health}\n"
+                      f"Weapons: {hero.weapon}\n"
+                      f"Armor: {hero.armor}\n"
+                      f"Inventory: {hero.inventory}")
+                continue
             hero.move(dir, room1)
             if hero.location == room1.room_exit:
                 break
