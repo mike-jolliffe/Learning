abf340d1e504dfe69c8f88c563cb196678884c9f
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-09-12 20:12:30 -0700

    resolved bug with 3 hop cumulative distance in hop_times(). Got rid of dictionary approach and just appended to an array as I looped through each hop

--- PDX_Code_Guild/road_trip/road_trip.py
+++ PDX_Code_Guild/road_trip/road_trip.py
@@ -3,7 +3,7 @@
 * When the user enters a city and a number of hops, print out the shortest travel times to each city.
 Paths with fewer hops are not necessarily those with the shorter travel times.'''
 
-import itertools
+#import itertools
 
 class CityHopper:
     '''Allows user to check shortest routes and potential cities based on a starting city
@@ -51,74 +51,52 @@ class CityHopper:
         # Build set of all possible destinations
         all_dest = set([city[2] for city in city_list])
 
-        # Build travel times dictionary tied to hop number
-        hop_time_dict = {}
+        # Create final array
+        final_array = []
+        # Initialize a loop array with starting city and zero miles traveled
+        loop_array = [[self.start_city, 0]]
+        # Initialize a temporary holder
+        loop_holder = []
+        hop_count = 1
+        while hop_count <= self.num_hops:
+            # Grab all the 'from' cities from the last hop
+            for from_city in loop_array:
+                # Use those from cities in the loop array to look up possible destinations and times this hop
+                values = self.city_dict[from_city[-2]]
+                # For each potential destination city
+                for destination in values:
+                    # Grab all prior from cities (i.e., the path of hops so far)
+                    path = from_city[:-1]
+                    # Append the potential destination to the path
+                    path.append(destination)
+                    # Append the total travel time across all hops to the path
+                    path.append(values[destination] + from_city[-1])
+                    # Save the path with its associated cumulative travel time to the final array
+                    final_array.append(path)
+                    # Save the path to the temporary holder so that loop array so it can be used in the next loop
+                    loop_holder.append(path)
+            # Reset loop array so that we can start from our last hop on the next hop
+            loop_array = loop_holder
+            # Empty out the loop array so it's ready for next iteration
+            loop_holder = []
+            # Update the hop count
+            hop_count += 1
+        return all_dest, final_array
 
-        max_hop_num = max([city[0] for city in city_list])
-        hop_countdown = max_hop_num
-        hop_num = 1
-
-        for cities in city_list:
-            cities[0] = abs(cities[0] - (max_hop_num + 1))
-
-        # Put individual travel time pairs into dictionary
-        while hop_num <= max_hop_num:
-            for city in city_list:
-                if city[0] == hop_num and not hop_num in hop_time_dict:
-                    hop_time_dict[hop_num] = [[city[1], city[2], city[3]]]
-                elif city[0] == hop_num:
-                    hop_time_dict[hop_num].append([city[1], city[2], city[3]])
-                else:
-                    pass
-            hop_num += 1
-
-        # TODO resolve the bug in this part of the code that makes the 3-hop total too high
-        # For all keys > 1 in dictionary, grab the travel time from the previous key and add to current travel time
-        key_count = sorted([key for key in hop_time_dict.keys()])
-        # For a given hop number
-        for hop in key_count:
-            # For each city pair within the hop number
-            for final_cities in hop_time_dict[hop]:
-                # The first city in that pair is the from city
-                from_city = final_cities[0]
-                # If we are not in the first hop
-                if not hop == 1:
-                    # Look into the prior hop's cities
-                    for interim_cities in hop_time_dict[hop-1]: # TODO resolve whether I need a loop here, maybe not
-                        # When a prior hop's to city matches current from city
-                        if interim_cities[1] == from_city:
-                            print(f"Departure city: {interim_cities[1]}, time so far: {interim_cities[2]}, "
-                                  f"{from_city}, new distance: {final_cities[2]}")
-                        # Modify the higher key's city distance
-                            final_cities[2] += interim_cities[2]
-                            print(f" New total dist: {final_cities[2]}")
-                else:
-                    break
-
-            hop += 1
-
-        # Clean up the dictionary by removing the from city, leaving only destination and cumulative distance
-        for keys in hop_time_dict:
-            for key in hop_time_dict[keys]:
-                del key[0]
-
-        return all_dest, hop_time_dict
-
-
-    def all_times(self, all_destinations, hop_time_dict):
+    def all_times(self, all_destinations, hop_times):
         '''Returns all cities that can be reached and their various travel times via different hops.'''
         # Create a dictionary that will hold cumulative times by destination
         cumulative_times = {}
         # For each final destination city
         for city in all_destinations:
             # Get the distance from the hop time dictionary for all hops with this city as the destination
-            time_array = [[destination[1] for destination in value if destination[0] == city] for key, value in hop_time_dict.items()]
+            time_array = [destination[-1] for destination in hop_times if destination[-2] == city]
             # Add the city and various times to the cumulative times dictionary
             if not city in cumulative_times:
                 cumulative_times[city] = time_array
-        # Flatten the values in the dictionary
-        for key in cumulative_times:
-            cumulative_times[key] = list(itertools.chain.from_iterable(cumulative_times[key]))
+        # Flatten the value lists in the dictionary
+        #for key in cumulative_times:
+        #    cumulative_times[key] = list(itertools.chain.from_iterable(cumulative_times[key]))
 
         return cumulative_times
 
@@ -137,11 +115,13 @@ if __name__ == '__main__':
     hopper = CityHopper()
     # Grab starting city and number of hops from user
     start_city, num_hops = hopper.get_input()
-    # Get set of hoppable cities
+    # Get set of hoppable cities given hop number constraint
     city_list = hopper.can_hop(start_city, num_hops)
+    # Return all hop times to potential destinations for all possible routes
     all_destinations, hop_times = hopper.hop_times(city_list)
     all_times = hopper.all_times(all_destinations, hop_times)
     print(f"\nStarting from {hopper.start_city} and given {hopper.num_hops} hops, you can travel to:\n")
+    # Report the minimum times for each possible destination
     min_times = hopper.min_time(all_times)
     for city in min_times:
         print(f"{city} in {min_times[city]} hours")
