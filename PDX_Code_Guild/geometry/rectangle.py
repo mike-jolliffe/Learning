c6a7c5359dcc69c9a961e2c9c55fc3d7c1ba1843
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-09-19 19:34:06 -0700

    modified rectangle so point_inside() works

--- PDX_Code_Guild/geometry/rectangle.py
+++ PDX_Code_Guild/geometry/rectangle.py
@@ -1,22 +1,18 @@
-import point
+from point import Point
 
 class Rectangle(Point):
     def __init__(self, x, y, width, height):
-        super.__init__(self, x, y)
+        super().__init__(x,y)
         self.width = width
         self.height = height
-        self.corners = []
-
-    def make_corners(self):
-        '''Updates self.corners so it holds the point locations of rectangle corners'''
-        self.corners.append((self.x, self.y),
-                            (self.x, self.y - height),
-                            (self.x + width, self.y),
-                            (self.x + width, self.y - height))
+        self.corners = [(self.x, self.y),
+                        (self.x, self.y - self.height),
+                        (self.x + self.width, self.y),
+                        (self.x + self.width, self.y - self.height)]
 
     def __repr__(self):
         return f"This rectangle starting at ({self.x}, {self.y}), with height {self.height} and width {self.width}"\
-               f" is composed of the following points: {corner for corner in self.corners}"
+               f" is composed of the following points: {[corner for corner in self.corners]}"
 
     def __eq__(self, other):
         if self.corners == other.corners:
@@ -31,10 +27,15 @@ class Rectangle(Point):
 
     def point_inside(self, point):
         '''Given a Point object, returns True if it falls within the rectangle '''
-        max_x, min_x = [max(points), min(points) for points[0] in self.corners]
-        max_y, min_y = [max(points), min(points) for points[1] in self.corners]
+        max_x, min_x = max([points[0] for points in self.corners]), min([points[0] for points in self.corners])
+        max_y, min_y = max([points[1] for points in self.corners]), min([points[1] for points in self.corners])
 
         if point.x in range(min_x, max_x + 1) and point.y in range(min_y, max_y + 1):
             return True
         else:
             return False
+
+if __name__ == '__main__':
+    new_rectangle = Rectangle(0,4,5,5)
+    print(new_rectangle.point_inside(Point(100,3)))
+    print(new_rectangle)
