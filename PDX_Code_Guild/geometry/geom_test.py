1a89ae67fe98eda41dd8ad6ea2fa84953113cd13
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-09-20 12:26:06 -0700

    added tests for Rectangle() class and successfully tested

--- PDX_Code_Guild/geometry/geom_test.py
+++ PDX_Code_Guild/geometry/geom_test.py
@@ -1,5 +1,6 @@
 import unittest
 from point import Point
+from rectangle import Rectangle
 import rectangle
 
 
@@ -23,6 +24,29 @@ class TestPoints(unittest.TestCase):
     def test_move(self):
         self.assertEqual(self.point1.move((1,1)), '(1, 1)')
         self.assertEqual(self.point3.move((-1,-1)), '(-1, -1)')
+
+class TestRects(unittest.TestCase):
+    def setUp(self):
+        self.rect1 = Rectangle(0, 5, 5, 5)
+        self.rect2 = Rectangle(0, 5, 5, 5)
+        self.rect3 = Rectangle(1, 6, 6, 6)
+
+    def test_repr(self):
+        self.assertEqual(self.rect1.__repr__(), f"This rectangle starting at (0, 5), with height 5 and width 5"\
+               f" is composed of the following points: [(0, 5), (0, 0), (5, 5), (5, 0)]")
+
+    def test_eq(self):
+        self.assertEqual(self.rect1 == self.rect2, True)
+        self.assertNotEqual(self.rect1 == self.rect3, True)
+
+    def test_find_center(self):
+        self.assertEqual(self.rect1.find_center(), (2.5, 2.5))
+
+    def test_point_inside(self):
+        self.assertTrue(self.rect1.point_inside(Point(2,2)))
+        self.assertFalse(self.rect1.point_inside(Point(10,10)))
+
+
 
 if __name__ == '__main__':
     unittest.main()
