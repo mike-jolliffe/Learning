2cd67a6fa783b6e05957eaced8992dd90252cda6
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-09-29 10:09:01 -0700

    added fee charging functionality to Account class

--- PDX_Code_Guild/atm-interface/account.py
+++ PDX_Code_Guild/atm-interface/account.py
@@ -1,10 +1,6 @@
 '''
 Adds some advanced features to the account class.
 
-*   Add a function called `get_standing()` have it return a bool with whether the account has less than $1000 in it.
-
-*   Predatorily charge a transaction fee every time a withdrawal or deposit happens if the account is in bad standing.
-
 *   Save the account balance to a file after each operation.
     Read that balance on startup so the balance persists across program starts.
 
@@ -19,6 +15,7 @@ class Account:
         self.account_number = Account.account_number
         self.__balance = 0
         self.__interest = 0.001
+        self.fee = 0
 
         Account.account_number += 1
 
@@ -31,16 +28,19 @@ class Account:
         return self.__balance
 
     def deposit(self, amount):
-        '''Updates balance by adding amount'''
-        self.__balance += amount
+        '''Updates balance by adding amount minus fee'''
+        self.fee_charge()
+        self.__balance += (amount - self.fee)
         return self.__balance
 
     def check_withdrawal(self, amount):
-        '''Returns True if balance greater than or equal to amount'''
-        return True if self.__balance >= amount else False
+        '''Returns True if balance greater than or equal to amount plus any fee'''
+        self.fee_charge()
+        return True if self.__balance >= (amount + self.fee) else False
 
     def withdraw(self, amount):
         '''Withdraws an allowed amount, ValueError if insufficient balance'''
+        self.fee_charge()
         if self.__balance >= amount:
             self.__balance -= amount
             return self.__balance
@@ -50,6 +50,20 @@ class Account:
     def calc_interest(self):
         '''Calculates and returns total interest on loan balance'''
         return self.__balance * self.__interest
+
+    def get_standing(self):
+        '''Returns True if account balance is below $1000'''
+        return True if self.__balance < 1000 else False
+
+    def fee_charge(self):
+        '''Creates a fee charge if get standing returned True'''
+        if self.get_standing():
+            self.fee = 10
+        else:
+            self.fee = 0
+
+
+
 
 if __name__ == '__main__':
     account1 = Account()
