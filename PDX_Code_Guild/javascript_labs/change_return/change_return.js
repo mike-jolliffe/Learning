46a5b25a7408dd918ed2958502dfa2007b3e0163
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-10-03 12:30:38 -0700

    practicing JS functions in browser

---
+++ PDX_Code_Guild/javascript_labs/change_return/change_return.js
@@ -0,0 +1,67 @@
+
+
+function transform(input) {
+  if (input) {
+    clearError();
+    // Calculate the number of quarters necessary first.
+    var running_amt = parseInt(input);
+
+    var numQ = Math.floor(running_amt / 25);
+    running_amt = running_amt % 25;
+
+    // Calculate the number of dimes
+    var numD = Math.floor(running_amt / 10);
+    running_amt = running_amt % 10;
+
+    // Number of nickles
+    var numN = Math.floor(running_amt / 5);
+    running_amt = running_amt % 5;
+
+    // Number of pennies
+    var numP = Math.floor(running_amt / 1);
+
+    // Return minimum number of coins for user
+    var result = String(numQ + numD + numN + numP);
+    printResult(result);
+  }
+  else {
+    printError('Enter a value');
+    focusInput();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function (evt) {
+
+  setup(transform);
+  focusInput();
+});
+
+
+function focusInput() {
+  document.querySelector('[name="input"]').focus();
+}
+
+function printResult(str) {
+  $('#result').html(str);
+}
+
+function printError(str) {
+  var err = document.getElementById('error');
+  err.classList.add('error');
+  err.innerHTML = str;
+}
+
+function clearError() {
+  var err = document.getElementById('error');
+  err.innerHTML = '';
+  err.classList.remove('error');
+}
+
+function setup(callback) {
+  document.querySelector('form')
+    .addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    var value = document.querySelector('input').value;
+    callback(value);
+  });
+}
