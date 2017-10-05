e993f7a9d292025287db4f7f173c4a6855204e4b
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-10-04 22:39:33 -0700

    built targeting of radio buttons, dynamic addition to aside

--- PDX_Code_Guild/javascript_labs/burrito_order/js/app.js
+++ PDX_Code_Guild/javascript_labs/burrito_order/js/app.js
@@ -1,6 +1,19 @@
+var ingredList = [];
+$('.ui.checkbox').checkbox();
 
-// Grab the type of tortilla
-if ($('input[type="radio"]').is(':checked')) {
-    console.log($('input[value]').val());
-}
+
+// Grab the type of tortilla on click
+$('.ui.radio').click(function () {
+    if ($('input[type="radio"]').is(':checked')) {
+        var ingred = $(this).find('input[value]').val();
+        ingredList[0] = '<li>' + ingred + '</li>';
+        console.log(ingredList);
+        // Put the tortilla type into the aside
+        //$('.ingredients.ui.relaxed.list').unshift(ingredList);
+        console.log($('#ingredients .ui.relaxed.list').html(ingredList));
+    }
+});
+
+
+
 
