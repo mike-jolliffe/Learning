6a988ed0eec82c0c37727264a9276a956b6ba1ab
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-10-05 12:13:29 -0700

    fixed overlap of jQuery selectors

--- PDX_Code_Guild/javascript_labs/burrito_order/js/app.js
+++ PDX_Code_Guild/javascript_labs/burrito_order/js/app.js
@@ -1,12 +1,14 @@
+//TODO get default checked fields
+
 var ingredList = [];
 $('.ui.checkbox').checkbox();
 
 
 // Grab the type of tortilla on click
-$('.ui.radio').click(function () {
-    if ($('input[name="tortilla"]').is(':checked')) {
-        var ingred = $(this).find('input[value]').val();
-        ingredList[0] = '<li>' + ingred + '</li>';
+$('.fields:has(label[for="tortilla"]) .ui.radio').change(function () {
+    if ($(this).find('input').prop('checked') == true) {
+        var ingred = $(this).find('label').html();
+        ingredList[0] = ingred;
         console.log(ingredList);
         // Put the tortilla type into the aside
         //$('.ingredients.ui.relaxed.list').unshift(ingredList);
@@ -15,32 +17,21 @@ $('.ui.radio').click(function () {
 });
 
 // Grab type of meat and additionals
-$('.ui.checkbox').change(function() {
+$('.fields:has(label[for="meat"]) .ui.checkbox').change(function() {
     if ($(this).find('input').prop('checked') == true) {
        // It is not checked, show your div...
        ingredList.push($(this).find('label').html());
    } else if ($(this).find('input').prop('checked') == false) {
        var ingred = $(this).find('label').html();
-       var remove_ix = ingredList.indexOf('<li>' + ingred + '</li>');
+       var remove_ix = ingredList.indexOf(ingred);
        console.log(remove_ix);
-       ingredList.splice(remove_ix);
+       if (remove_ix > -1) {
+           ingredList.splice(remove_ix, 1);
+       }
    }
    console.log(ingredList);
 });
 
-// $('.ui.checkbox').click(function () {
-//     if ($('input[name="meat"]').is(':checked')) {
-//         var ingred = $(this).find('input[value]').val();
-//         ingredList.push('<li>' + ingred + '</li>');
-//     } else if ($('input[type="checkbox"]').is(':not(:checked)')) {
-//         var ingred = $(this).find('input[value]').val();
-//         console.log(ingred);
-//         var remove_ix = ingredList.indexOf('<li>' + ingred + '</li>');
-//         console.log(remove_ix);
-//         ingredList.splice(remove_ix);
-//     }
-//     console.log(ingredList);
-// });
 
 
 
