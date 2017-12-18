bcb3787eecb3d73ab5f29526eb31ff02580e5c75
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-12-18 11:49:12 -0800

    resolved btrees

--- mergeBTrees.py
+++ mergeBTrees.py
@@ -4,70 +4,38 @@ class TreeNode(object):
         self.val = x
         self.left = None
         self.right = None
-        self.hasChildren = self.hasChildren()
+        # self.hasChildren = self.hasChildren()
 
     def __str__(self):
         return str(self.val)
 
-    def hasChildren(self):
-        """
-        :type node: TreeNode
-        :rtype: boolean
-        """
-        # Check if t1 and t2 have children
-        if self.left == None and self.right == None:
-            print("no children for this node")
-            return False
-        else:
-            print("node found")
-            return True
-
 
 class Solution(object):
+
+    def __init__(self):
+        self.node_vals = []
+
     def mergeTrees(self, t1, t2):
         """
         :type t1: TreeNode
         :type t2: TreeNode
         :rtype: TreeNode
         """
-        node_vals = []
 
         if t1 == t2 == None:
             return None
         elif t1 == None:
-            if t1.hasChildren == t2.hasChildren == False:
-            node_vals.append(t1.val + t2.val)
-            return ("Bottom of this branch!")
-
-        elif t1.hasChildren == False:
-            node_vals.append(t1.val + t2.val)
+            self.node_vals.append(t2.val)
             return self.mergeTrees(None, t2.left), self.mergeTrees(None, t2.right)
-
-        elif t2.hasChildren == False:
-            node_vals.append(t1.val)
+        elif t2 == None:
+            self.node_vals.append(t1.val)
             return self.mergeTrees(t1.left, None), self.mergeTrees(t1.right, None)
-
         else:
-            node_vals.append(t1.val + t2.val)
+            self.node_vals.append(t1.val + t2.val)
             return self.mergeTrees(t1.left, t2.left), self.mergeTrees(t1.right, t2.right)
 
-
-    # def addNodes(self, t1, t2):
-    #     """
-    #     :type t1: TreeNode
-    #     :type t2: TreeNode
-    #     :rtype: integer
-    #     """
-    #
-    #     if t1.val == None:
-    #         print(t1.val)
-    #         return t2
-    #     elif t2.val == None:
-    #         print(t2.val)
-    #         return t1
-    #     else:
-    #         print(t1.val + t2.val)
-    #         return TreeNode(t1.val + t2.val)
+    def toConsole(self):
+        return self.node_vals
 
 if __name__ == "__main__":
     node1 = TreeNode(2)
@@ -79,4 +47,5 @@ if __name__ == "__main__":
     node1.right = node11
     node2.left = node20
     sol = Solution()
-    print(sol.mergeTrees(node1, node2))
+    sol.mergeTrees(node1, node2)
+    print(sol.node_vals)
