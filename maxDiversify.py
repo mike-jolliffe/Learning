353690a9e9cc08fc11d49de8a6936f94f66e4f89
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-12-22 14:09:59 -0600

    daily coding challenge

---
+++ maxDiversify.py
@@ -0,0 +1,17 @@
+class Solution:
+    def distributeCandies(self, candies):
+        """ Split candies evenly across two individuals, maximizing diversity of
+            candy type given to one of the individuals, while maintaining equal
+            number of candies to each.
+        :type candies: List[int]
+        :rtype: int
+        """
+
+        # Create candy counter
+
+        # Calculat half of total candies
+
+        # Get set candies
+
+        # While candy counter less than half all candies
+            # For candy in set, candy counter += 1
