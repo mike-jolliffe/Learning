0f63fef063370a5026ef66f5eba107fd1baabb26
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-12-28 17:01:48 -0800

    daily coding challenge

---
+++ leet_521.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findLUSlength(self, a, b):
+        """
+        Given a group of two strings, finds the longest uncommon
+        subsequence of this group of two strings.
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+
+        # Create placeholder string
+
+        # Max length
+
+        # Current length
+
+        # Zip the two strings for pairwise comparison
+
+        # If the values at a given index are same, is 0
+
+        # If the values at a given index different, is 1
+
+        # Iterate through string, incrementing current length until 0
+
+        # If current length > max length, new max length
+
+        # Reset current length, continue until end of string 
