14a9f27525381a0750ab9f46b21f6ef274f5e3b9
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-12-28 17:17:19 -0800

    daily coding challenge

--- leet_521.py
+++ leet_521.py
@@ -8,20 +8,10 @@ class Solution:
         :rtype: int
         """
 
-        # Create placeholder string
-
-        # Max length
-
-        # Current length
-
-        # Zip the two strings for pairwise comparison
-
-        # If the values at a given index are same, is 0
-
-        # If the values at a given index different, is 1
-
-        # Iterate through string, incrementing current length until 0
-
-        # If current length > max length, new max length
-
-        # Reset current length, continue until end of string 
+        # Max length tracker
+        # For sequence start in first string a
+            # For sequence end in range a+1 till end of a
+                # Length tracker
+                # If the sequence is not in b
+                    # Check length tracker against max length
+        # return max length tracker if max length tracker > 0 else -1
