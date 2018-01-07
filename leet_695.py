35de97eb0a84664c217d14d73c3b3c95da850811
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-01-07 12:26:08 -0800

    daily coding challenge -- find area of biggest island in 2D array

--- leet_695.py
+++ leet_695.py
@@ -1,4 +1,8 @@
 class Solution:
+
+    def __init__(self):
+        self.island_dict = None
+
     def maxAreaOfIsland(self, grid):
         """Returns the area of largest island in grid, counting connectivity
            in the four cardinal directions
@@ -7,26 +11,22 @@ class Solution:
         """
 
         # Convert grid to x,y dict
-        island_dict = self.gridToDict(grid)
+        self.island_dict = self.gridToDict(grid)
         # Create max_area holder
         max_area = 0
+        area = 0
         # for each dict key
-        for key in island_dict:
-            curr_area = 0
+        for key in self.island_dict:
             # If key has value one
-            if island_dict[key] == 1:
-                # change its value to 2
-                island_dict[key] = 2
-                # Increment curr_area by 1
-                curr_area += 1
-                # repeat in all four directions until no 1s found
-                addtl_area = self.Search(key)
+            if self.island_dict[key] == 1:
+                # search in all four directions until no 1s found
+                area = self.Search(self.island_dict, key)
+                print("Area of islands: {}".format(area))
             else:
                 # Check curr_area vs. max_area
-                if curr_area > max_area:
-                    max_area = curr_area
-                # Set curr_area to zero
-                curr_area = 0
+                if area > max_area:
+                    max_area = area
+        return max([max_area, area])
 
     def gridToDict(self, grid):
         """Returns dictionary representation of 2D array, with keys x-y coords
@@ -41,15 +41,40 @@ class Solution:
 
         return xyDict
 
-    def Search(self, key):
+    def Search(self, xymap, start, tot_area=0):
         """Returns total number of 1s connected four-directionally to given
            starting point (x,y coord)
-        :type key: tuple of x,y coords
+        :type map: dictionary of x,y coord keys and 1/0 values
+        :type start: tuple of x,y coords
         :rtype: int
         """
+        try:
+            #print("Location: {}, value: {}".format(start, xymap[start]))
+            if xymap[start] == 0 or xymap[start] == 2:
+                return tot_area
+            elif xymap[start] == 1:
+                tot_area += 1
+                #print("Current total area: {}".format(tot_area))
+                xymap[start] = 2
 
-        
+                up = self.Search(xymap, (start[0], start[1] - 1), tot_area)
+                down = self.Search(xymap, (start[0], start[1] + 1), tot_area)
+                left = self.Search(xymap, (start[0] - 1, start[1]), tot_area)
+                right = self.Search(xymap, (start[0] + 1, start[1]), tot_area)
+
+                #print(tot_area, up, down, left, right)
+                #print(max([tot_area, up, down, left, right]))
+                return max([up, down, left, right])
+
+        except KeyError:
+            #print("Location {} is out of bounds".format(start))
+            return tot_area
+
+
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.gridToDict([[0,0,1,0],[0,0,1,1]]))
+    #dictionary = sol.gridToDict([[0,0,1,0],[0,0,1,1]])
+    #print(sol.maxAreaOfIsland([[0,0,1,0],[0,0,1,1]]))  # 3
+    print(sol.maxAreaOfIsland([[1,0],[1,1]]))  # 3
+    #print(sol.maxAreaOfIsland([[1]]))  # 1
