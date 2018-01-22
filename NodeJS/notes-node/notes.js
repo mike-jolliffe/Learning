ff454c846cb8fcb6d0ffbe56d02d8f78c30e43b5
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-01-22 15:18:46 -0800

    create addNote functionality, check for unique note titles

--- NodeJS/notes-node/notes.js
+++ NodeJS/notes-node/notes.js
@@ -1,7 +1,31 @@
+// notify that app is running
 console.log('Starting notes.js');
 
+// File IO functionality
+const fs = require('fs');
+
+// Create an array containing a note object, write to file
 var addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  var notes = [];
+  // Note object
+  var note = {
+    title,
+    body
+  }
+
+  try {
+    var notesString = fs.readFileSync('notes.json');
+    notes = JSON.parse(notesString);
+  } catch(e){
+    // Pass
+  }
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes.json', JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
