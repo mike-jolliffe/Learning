28032c4fe42a14a02ef03c8e7776f33faeb1f3c6
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-01-22 15:46:02 -0800

    refactored note fetching and saving functionality into their own functions

--- NodeJS/notes-node/app.js
+++ NodeJS/notes-node/app.js
@@ -10,7 +10,12 @@ const argv = yargs.argv;
 var command = argv._[0];
 
 if (command === 'add') {
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log(`Created the note:\n${note.title} -- ${note.body}`);
+  } else {
+    console.log('There was a problem creating the note!!');
+  }
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read') {
--- NodeJS/notes-node/notes.js
+++ NodeJS/notes-node/notes.js
@@ -4,27 +4,36 @@ console.log('Starting notes.js');
 // File IO functionality
 const fs = require('fs');
 
+// Fetch all notes from file
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes.json');
+    return JSON.parse(notesString);
+  } catch(e){
+    return [];
+  }
+};
+
+// Save all notes to file
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes.json', JSON.stringify(notes));
+}
+
 // Create an array containing a note object, write to file
 var addNote = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   // Note object
   var note = {
     title,
     body
   }
 
-  try {
-    var notesString = fs.readFileSync('notes.json');
-    notes = JSON.parse(notesString);
-  } catch(e){
-    // Pass
-  }
-
   var duplicateNotes = notes.filter((note) => note.title === title);
 
   if (duplicateNotes.length === 0) {
     notes.push(note);
-    fs.writeFileSync('notes.json', JSON.stringify(notes));
+    saveNotes(notes);
+    return note;
   }
 };
 
