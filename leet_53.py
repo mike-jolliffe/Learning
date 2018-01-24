3f55a63e9979b281392503825f4228240837f5ea
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-01-24 11:12:20 -0800

    fixing corner cases

--- leet_53.py
+++ leet_53.py
@@ -4,15 +4,23 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-
-        subarray_max = 0
-        for i in range(len(nums) - 2):
-            for j in range(i + 1, (len(nums) - 1)):
+        if len(nums) == 1:
+            return nums[0]
+        subarray_max = nums[0]
+        for i in range(len(nums) - 1):
+            if nums[i] > subarray_max:
+                subarray_max = nums[i]
+            for j in range(i + 1, (len(nums))):
+                if nums[j] > subarray_max:
+                    subarray_max = nums[j]
                 if sum(nums[i:j+1]) > subarray_max:
                     subarray_max = sum(nums[i:j+1])
-                    #print(nums[i:j+1])
+                    #print(nums[i:j])
         return subarray_max
 
 if __name__ == '__main__':
     sol = Solution()
     print(sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))  # 6
+    print(sol.maxSubArray([1]))  # 1
+    print(sol.maxSubArray([-2,1]))  # 1
+    print(sol.maxSubArray([1,2]))  # 3
