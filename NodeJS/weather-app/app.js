3b4d487d390638075e865cfaf2006063377a9c17
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-01-29 12:41:16 -0800

    refactored app.js by moving the address geocoding functionality into its own file

--- NodeJS/weather-app/app.js
+++ NodeJS/weather-app/app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
   .options({
@@ -13,19 +13,10 @@ const argv = yargs
   .help()
   .argv;
 
-var encAddress = encodeURIComponent(argv.a);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encAddress}`,
-  json: true
-},(error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Google servers.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
   }
 });
