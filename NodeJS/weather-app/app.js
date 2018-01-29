03de84392bd4de34b3132fa735be551ef484547a
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-01-29 13:18:56 -0800

    connecting to Dark Sky API for weather data

--- NodeJS/weather-app/app.js
+++ NodeJS/weather-app/app.js
@@ -1,22 +1,41 @@
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+//
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .argv;
+const request = require('request');
+const app_configs = require('./configs');
+const API_KEY = app_configs.API_KEY;
+const LAT = 45.677;
+const LNG = -115.445;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+
+request({
+  url: `https://api.darksky.net/forecast/${API_KEY}/${LAT},${LNG}`,
+  json: true
+},(error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather data.');
   }
 });
