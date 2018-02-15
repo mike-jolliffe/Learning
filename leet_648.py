0e45a46c163b4bf3aa2d12a9ad9903e9830fe54a
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-02-15 10:13:11 -0800

    daily coding challenge -- replace words in sentence with their roots taken from a lookup list

---
+++ leet_648.py
@@ -0,0 +1,23 @@
+class Solution:
+    def replaceWords(self, dictionary, sentence):
+        """Given dictionary of word roots, replace words in sentence with roots
+        :type dict: List[str]
+        :type sentence: str
+        :rtype: str
+        """
+
+        # Split the sentence
+        split_sentence = sentence.split()
+        # For each word in the sentence
+        for i in range(len(split_sentence)):
+            # If a root is part of that word
+            for root in dictionary:
+                if root in split_sentence[i]:
+                    # Replace that word
+                    split_sentence[i] = root
+        return ' '.join(split_sentence)
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.replaceWords(["cat", "rat", "bat"], "the cattle was rattled by the battery"))
