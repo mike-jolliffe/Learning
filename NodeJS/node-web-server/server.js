055a0dc2108254b7e109bd90710c257be23b4bd2
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-04-09 20:17:39 -0700

    render static template with node

--- NodeJS/node-web-server/server.js
+++ NodeJS/node-web-server/server.js
@@ -3,13 +3,15 @@ const express = require('express');
 // Instantiate new app
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 // Register handlers
 app.get('/', (req, res) => {
   res.send('<h1>Hello Express!</h1>');
 });
 
 app.get('/about', (req, res) => {
-  res.send("About page.");
+  res.send('About page.');
 });
 
 app.get('/bad', (req, res) => {
@@ -19,4 +21,6 @@ app.get('/bad', (req, res) => {
 });
 
 // Bind app to port on machine
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
