0a15463f6b1f8ca0299d88bb830088769ffc7801
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-04-11 19:09:13 -0700

    adding middleware for site maintenance and logging client requests

--- NodeJS/node-web-server/server.js
+++ NodeJS/node-web-server/server.js
@@ -1,13 +1,40 @@
 const express = require('express');
+// Import handlebars
 const hbs = require('hbs');
+const fs = require('fs');
 
 // Instantiate new app
 var app = express();
 
+// Allow partials in template
 hbs.registerPartials(__dirname + '/views/partials')
+// Make handlebars the engine
 app.set('view engine', 'hbs');
+
+// Middleware to log client requests to server
+app.use((req, res, next) => {
+  // log time, method, and url of user request on site to console
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.')
+    }
+  });
+  next();
+});
+
+// Middleware site maintenance. No next() so won't move on remainder of code.
+app.use((req, res, next) => {
+  res.render('maintenance.hbs')
+});
+
+// Middleware for static
 app.use(express.static(__dirname + '/public'));
 
+// Create helper functions for repeated use in templates
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 });
