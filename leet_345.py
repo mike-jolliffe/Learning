525cd50f76e4218ee5a2185b74c4a3123cb1eda5
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-04-21 19:52:45 -0700

    changed from dict to deque to maintain order of replacement operations in cases of same vowel

--- leet_345.py
+++ leet_345.py
@@ -1,20 +1,27 @@
+from collections import deque
+
+
 class Solution(object):
     def reverseVowels(self, s):
         """Return string with vowel order reversed
         :type s: str
         :rtype: str
         """
-        vowels_forward = [char for char in s.lower() if char in 'aeiou']
-        # Map vowels to reversed vowels
-        vowels_mapper = {forward: backward for forward, backward
-                        in zip(vowels_forward, reversed(vowels_forward))}
+        vowels_forward = [char for char in s if char.lower() in 'aeiou']
+        # Map vowels to reversed vowels into deque structure
+        vowels_mapper = deque([(forward, backward) for forward, backward
+                        in zip(vowels_forward, reversed(vowels_forward))])
         for i in range(len(s)):
             # If the char is a vowel
             if s[i] in vowels_forward:
+                # Grab first element from deque object (stack and queue generalized)
+                new_vowel = vowels_mapper.popleft()[1]
                 # Break string apart, swap vowel, put together
-                s = s[:i] + vowels_mapper[s[i]] + s[i+1:]
+                s = s[:i] + new_vowel + s[i+1:]
+
         return s
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.reverseVowels(''))
+    print(sol.reverseVowels('leetcode'))
+    print(sol.reverseVowels('hello'))
