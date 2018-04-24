c548763ef719cf847a8a6a0656026a116ce4648b
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-04-23 20:10:06 -0700

    making success-alert and warning-alert components

---
+++ Angular/my-dream-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'warning-alert',  // Select component by selector
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent {
+
+}
