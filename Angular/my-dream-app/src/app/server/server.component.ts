00f35511a7d2da9d66f2da8e2c5f319dc7a47c89
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-05-01 19:16:35 -0700

    using ngStyle directive

--- Angular/my-dream-app/src/app/server/server.component.ts
+++ Angular/my-dream-app/src/app/server/server.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
-  ServerID: number = 10;
-  ServerStatus: string = 'offline';
+  serverID: number = 10;
+  serverStatus: string = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online': 'offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green': 'red';
+  }
+
 }
