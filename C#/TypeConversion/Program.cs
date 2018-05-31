949627243bfc2c3de768512aa53275ee810c6d15
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-05-30 19:30:58 -0700

    type conversion and try/catch

---
+++ C#/TypeConversion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+
+            var number = "1234";
+            Convert.ToInt32(number);
+            Console.WriteLine(number);
+
+            try
+            {
+                var num2 = "1234";
+                Convert.ToByte(num2);
+                Console.WriteLine(num2);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
+
+        }
+    }
+}
