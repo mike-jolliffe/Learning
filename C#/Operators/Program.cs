dea22a82ac50ac934e1fd2ea18491fea9ed07aac
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-05-31 19:25:11 -0700

    operators and boolean expressions

---
+++ C#/Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            var c = 3;
+
+            Console.WriteLine(a / b);
+            Console.WriteLine(c > b && c < a);
+        }
+    }
+}
