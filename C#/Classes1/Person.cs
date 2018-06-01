f9222b90f0861d4cda66a9e0b37dfc50de8bd2f7
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-05-31 19:54:31 -0700

    classes

---
+++ C#/Classes1/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classes1
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + firstName + " " + lastName);
+        }
+    }
+
+}
---
+++ C#/Classes1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Classes1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.firstName = "John";
+            john.lastName = "Smith";
+            john.Introduce();
+        }
+    }
+}
