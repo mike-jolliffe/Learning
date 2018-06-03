267be24b70876873bfcdef2aed858a7a56cf894f
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-03 09:09:34 -0700

    arrays

---
+++ C#/Arrays1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Arrays1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var bools = new bool[3];
+            bools[0] = true;
+
+            Console.WriteLine(bools[0]);
+            Console.WriteLine(bools[1]);
+            Console.WriteLine(bools[2]);
+
+            var names = new string[3] { "Mary", "John", "Jack" };
+        }
+    }
+}
