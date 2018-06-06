4011202411767b7caa2b6e8dd395631d7f457805
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-05 18:33:29 -0700

    working with enums

---
+++ C#/Enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        ExpressMail = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.ExpressMail;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "RegisteredAirMail";
+            var shippingMethod = Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(shippingMethod);
+        }
+    }
+}
