04f576a86a20661d09df25bdd741cbcce2b89225
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-11 19:23:33 -0700

    Get the maximum of two input numbers

---
+++ C#/getMax/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace getMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a first number: ");
+            var firstNum = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pick a second number: ");
+            var secondNum = Int32.Parse(Console.ReadLine());
+
+            var max = (firstNum > secondNum) ? firstNum : secondNum;
+            Console.WriteLine(max + " is the maximum number.");
+        }
+    }
+}
