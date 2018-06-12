20d3618ef90ed4224e1a9f59a7dc26eed74a6c6b
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-11 18:30:54 -0700

    check if user input the a number between 1 and 10, inclusive. Assumes only a number as input

---
+++ C#/NumCheck/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NumCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 10");
+            var input = Int32.Parse(Console.ReadLine());
+
+
+            if(input > 0 && input < 11)
+            {
+                Console.WriteLine("VALID");
+            }
+            else 
+            {
+                Console.WriteLine("INVALID");
+            }
+
+        }
+    }
+}
