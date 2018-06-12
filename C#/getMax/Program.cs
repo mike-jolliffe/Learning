9d527e3b7cccc1bd7ab8471eb51a6eb9a438cd54
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-11 19:27:26 -0700

    added ternary with string output for orientation of photo

--- C#/getMax/Program.cs
+++ C#/getMax/Program.cs
@@ -14,6 +14,16 @@ namespace getMax
 
             var max = (firstNum > secondNum) ? firstNum : secondNum;
             Console.WriteLine(max + " is the maximum number.");
+
+            Console.WriteLine("Pick a width: ");
+            var width = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pick a height: ");
+            var height = Int32.Parse(Console.ReadLine());
+
+            var orientation = (width > height) ? "landscape" : "portrait";
+            Console.WriteLine("The photo is in " + orientation + " orientation");
+
         }
     }
 }
