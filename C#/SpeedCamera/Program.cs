3ff2cc00616617b542da5b219e1ebb6da248261f
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-12 18:23:27 -0700

    more C# practice with conditional flow

---
+++ C#/SpeedCamera/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SpeedCamera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a speed limit: ");
+            var speedLimit = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vehicle speed: ");
+            var carSpeed = Int32.Parse(Console.ReadLine());
+
+            if (carSpeed < speedLimit)
+            {
+                Console.WriteLine("OK"); 
+            }
+            else if (carSpeed - speedLimit >= 60)
+            {
+                Console.WriteLine("License Suspended");
+            }
+            else
+            {
+                var overLimitPoints = (carSpeed - speedLimit) / 5;
+                Console.WriteLine(overLimitPoints + " points on license.");
+            }
+        }
+    }
+}
