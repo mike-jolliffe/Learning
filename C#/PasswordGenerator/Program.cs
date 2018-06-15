672998706fc7de17450839166f846b83c90ef678
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-14 18:56:47 -0700

    use Random class to generate password

---
+++ C#/PasswordGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            const int passwordLength = 10;
+
+            var buffer = new char[passwordLength];
+
+            for (var i = 0; i < passwordLength; i++)
+                // Generate random lower-case letter from ASCII value
+                buffer[i] = (char)('a' + random.Next(0,26));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+        }
+    }
+}
