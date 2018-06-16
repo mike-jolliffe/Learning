c23965b7047ae5dd6097cd6b12eec4377077d26c
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-16 14:12:24 -0700

    find how many numbers between 1 and 100 are evenly divisible by 3

---
+++ C#/DivisibleByThree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DivisibleByThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter + " numbers are evenly divisible by 3.");
+        }
+    }
+}
