3da78162b4a513d441363b6199e60a9e808b06ff
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-16 14:18:43 -0700

    calculate sum of all user inputs

---
+++ C#/Summer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Summer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var total = 0;
+
+            while (true)
+            {
+                Console.Write("Please enter a number: ");
+                var val = Console.ReadLine();
+
+
+                if (val == "ok")
+                {
+                    break;
+                }
+                else
+                {
+                    total = total + Int32.Parse(val);  
+                }
+            }
+
+            Console.WriteLine("The total is " + total);
+        }
+    }
+}
