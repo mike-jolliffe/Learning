a57273d542ff89b0067da55fafb5c34d7a7d3b1a
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-16 14:27:32 -0700

    calculate factorial of a number

---
+++ C#/Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pick a number: ");
+            var fact = Int32.Parse(Console.ReadLine());
+            var total = 1;
+
+            for (int i = 1; i <= fact; i++)
+            {
+                total = total * i;
+            }
+            Console.WriteLine(fact + "! is " + total);
+
+            var simple_fact = Factorial(fact);
+            Console.WriteLine(simple_fact);
+        }
+    }
+}
