88889708464a3e671225ed52e9958e9010315c76
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-16 14:57:22 -0700

    parse input of comma-separated number values, display max

---
+++ C#/MaxOfVals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MaxOfVals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a group of comma-separated numbers: ");
+            var intString = Console.ReadLine();
+
+            string[] nums = intString.Split(",");
+            var max = 0;
+            foreach (var num in nums)
+            {
+                if (Int32.Parse(num) > max)
+                {
+                    max = Int32.Parse(num);
+                }
+            }
+
+            Console.WriteLine("The max is " + max);
+        }
+    }
+}
