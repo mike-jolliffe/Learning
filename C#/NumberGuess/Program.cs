ce41b055a063c5083e2eec363be681c9d9c4c94e
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-16 14:47:59 -0700

    guess a randomly generated number btwn 1-10

---
+++ C#/NumberGuess/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace NumberGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var ans = rnd.Next(1, 10);
+
+            while (true)
+            {
+
+                Console.Write("Guess a number between 1 and 10, inclusive: ");
+                var guess = Int32.Parse(Console.ReadLine());
+
+                if (guess == ans)
+                {
+                    Console.WriteLine("You won!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Guess again...");
+                }
+            }
+        }
+    }
+}
