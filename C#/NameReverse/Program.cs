d650cebb7c7b85111f4b911e8d16ef3cd332c35e
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-25 19:38:15 -0700

    reverse name using an Array

---
+++ C#/NameReverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NameReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+
+            var nameArray = new char[name.Length];
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                nameArray[i] = name[i];
+            }
+
+            Array.Reverse(nameArray);
+            Console.WriteLine(nameArray);
+
+            Console.WriteLine("Your name backwards is -- " + String.Join("", nameArray));
+
+        }
+    }
+}
