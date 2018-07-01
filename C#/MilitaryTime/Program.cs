42d22808f2972e479fb7d63fee4a5557c5112186
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-30 19:39:38 -0700

    check if user input string can be parsed to DateTime object

---
+++ C#/MilitaryTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MilitaryTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a time in military time format (e.g., 16:30 for 4:30pm): ");
+            var input = Console.ReadLine();
+
+            DateTime result;
+            var parsed = DateTime.TryParse(input, out result);
+
+            if (parsed)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Time");
+            }
+                                 
+        }
+    }
+}
