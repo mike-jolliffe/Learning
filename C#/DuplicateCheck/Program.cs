6385d88f7135666ad6dd7f179dc803d5826cfc10
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-30 19:31:15 -0700

    check for duplicates in set of hyphen-separated input values

---
+++ C#/DuplicateCheck/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DuplicateCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a set of hyphen-separated numbers: ");
+            var input = Console.ReadLine();
+            var nums = new List<int>();
+
+            if (!String.IsNullOrEmpty(input))
+            {
+                var inputSplit = input.Split("-");
+
+                foreach (var num in inputSplit)
+                {
+                    int result;
+                    var parsed = Int32.TryParse(num, out result);
+                    if (parsed)
+                    {
+                        nums.Add(result);
+                    }
+                }
+            }
+
+            if (Duplicates(nums))
+            {
+                Console.WriteLine("Duplicate");
+            }
+            else
+            {
+                Console.WriteLine("No Duplicates");
+            }
+
+        }
+
+        static bool Duplicates(List<int> nums)
+        {
+            var numSet = new HashSet<int>(nums);
+            // returns true if duplicates
+            return nums.Count != numSet.Count;
+
+        }
+    }
+}
