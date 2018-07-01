ee5ababe6af8bbec86e3790a768003522a58d092
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-30 18:29:43 -0700

    working with StringBuilder

---
+++ C#/StringBuilding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+
+            builder
+                .Append('!', 3)
+                .AppendLine()
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine();
+
+            builder.Replace('!', '?');
+
+            builder.Remove(15, 10);
+            var prepend = "Well ";
+            builder.Insert(0, prepend);
+
+            Console.WriteLine(builder);
+        }
+    }
+}
