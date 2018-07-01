43ce9aa97b71cd4cdbdc30195f4f339d5a699ad8
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-01 12:25:20 -0700

    display the number of vowels in input string

---
+++ C#/VowelCounter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VowelCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word: ");
+            var input = Console.ReadLine().ToCharArray();
+
+            var vowels = "aeiouAEIOU";
+            var counter = 0;
+
+            foreach(var letter in input)
+            {
+                if (vowels.Contains(letter.ToString()))
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+
+        }
+    }
+}
