1667d667424e0a42bd7e2163f48f3659ce0711aa
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-30 20:04:47 -0700

    convert user input words to a Pascal-cased variable

---
+++ C#/MakePascalVar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MakePascalVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a few words separated by spaces: ");
+            var input = Console.ReadLine().Split(" ");
+
+            var forPascal = new List<string>();
+            foreach(var word in input)
+            {
+
+                var firstLetter = word[0].ToString().ToUpper();
+                var allLower = word.ToLower();
+                var capFirst = char.ToUpper(allLower[0]) + allLower.Substring(1);
+                forPascal.Add(capFirst);
+
+            }
+
+            Console.WriteLine(string.Join("", forPascal));
+
+        }
+    }
+}
