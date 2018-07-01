bf018c86aa590a84f2ce5a6320c4d88b2522c2bc
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-06-30 17:41:45 -0700

    summarize text block by truncating to roughly given length, not cutting off mid-word

---
+++ C#/SummarizeText/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummarizeText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxLength = 20;
+            var sentence = "This is a really really really really long string.";
+
+            Console.WriteLine(StringUtility.Summarize(sentence, maxLength));
+
+        }
+    }
+}
---
+++ C#/SummarizeText/StringUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummarizeText
+{
+    public class StringUtility
+    {
+        public static string Summarize(string sentence, int maxLength)
+        {
+
+            if (sentence.Length < maxLength)
+            {
+                return sentence;
+            }
+
+            var words = sentence.Split(" ");
+            var charcount = 0;
+            var summaryWords = new List<string>();
+            foreach (var word in words)
+            {
+                charcount += word.Length + 1;
+                summaryWords.Add(word);
+                if (charcount >= maxLength)
+                {
+                    break;
+                }
+            }
+
+            return string.Join(" ", summaryWords) + " ...";
+
+        } 
+    }
+}
