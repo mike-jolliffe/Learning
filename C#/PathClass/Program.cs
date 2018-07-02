26872db238e0f1728ac1734daa26750932e37f17
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-01 20:14:30 -0700

    using Path for getting file name, directory, extension

---
+++ C#/PathClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace PathClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Projects\CSharpFundamentals\HelloWorld\HelloWorld.sln";
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+
+        }
+    }
+}
