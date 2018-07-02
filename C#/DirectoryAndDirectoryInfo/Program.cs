27319817954de8532547fdca1f40e301515d49ce
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-01 20:02:23 -0700

    working with directories

---
+++ C#/DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryAndDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\projects\CSharpFundamentals", "*.*", SearchOption.AllDirectories);
+            foreach(var file in files)
+            {
+                Console.WriteLine(file);  
+            }
+
+            var directories = Directory.GetDirectories(@"c:\projects\CSharpFundamentals", "*.*", SearchOption.AllDirectories);
+            foreach(var dir in directories)
+            {
+                Console.WriteLine(dir);
+            }
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
