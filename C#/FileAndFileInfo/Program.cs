4cdb50714c29e4e6d0e0e7903f7fb5ea8092d7fc
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-01 19:52:05 -0700

    working with Files

---
+++ C#/FileAndFileInfo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace FileAndFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(@"c:\tmp\somefile.jpg", @"d:\tmp\somefile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                // Do something
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                // Do something
+            }
+        }
+    }
+}
