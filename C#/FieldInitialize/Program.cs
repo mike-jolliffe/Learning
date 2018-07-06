0d7a4c29f0c00f6e9d3c90b89867ff3d7ba9b2d7
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-05 18:05:22 -0700

    exploring effects of readonly modifier on fields

---
+++ C#/FieldInitialize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FieldInitialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            Console.WriteLine(customer.Orders.Count);
+
+            customer.Promote();
+        }
+    }
+}
