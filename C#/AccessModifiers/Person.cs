2fd94038f45daa8c4a996a9f2ac266352385d3fe
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-05 18:22:37 -0700

    working with private access modifiers

---
+++ C#/AccessModifiers/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public Person()
+        {
+        }
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+}
---
+++ C#/AccessModifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate().ToString("MM.dd.yyyy"));
+        }
+    }
+}
