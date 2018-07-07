4af12e4663d0b7359d9b823f72a8a5bc8c052193
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-07 15:07:08 -0700

    create simplistic blog post, allow upvotes and downvotes

---
+++ C#/VoteForPost/Post.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VoteForPost
+{
+    public class Post
+    {
+        private string _Title;
+        private string _Description;
+        private DateTime _CreatedAt;
+        private int _UpVotes = 0;
+
+        public Post(string title, string description, DateTime createdAt)
+        {
+            this._Title = title;
+            this._Description = description;
+            this._CreatedAt = createdAt;
+        }
+
+        public Dictionary<string, string> GetPost()
+        {
+            var postDict = new Dictionary<string, string>
+            {
+                { "Post Title: ", _Title },
+                { "Description: ", _Description },
+                { "Created: ", _CreatedAt.ToString("MM-dd-yyyy") },
+                { "Votes: ", _UpVotes.ToString() }
+            };
+
+            return postDict;
+        }
+
+        public void Vote(string voteDirection)
+        {
+            if (voteDirection == "+" )
+            {
+                _UpVotes++;
+            }
+            else if (voteDirection == "-")
+            {
+                _UpVotes--;
+            }
+            else
+            {
+                throw new ArgumentException("Voting only accepts a [+] or [-]");
+            }
+        }
+    }
+}
---
+++ C#/VoteForPost/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VoteForPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post(
+                "First post", 
+                "Testing out post voting feature",
+                DateTime.Now
+            );
+
+            while (true)
+            {
+                foreach (var kvp in post.GetPost())
+                {
+                    Console.WriteLine("{0}{1}", kvp.Key, kvp.Value);
+                }
+
+                Console.WriteLine("Please upvote with a [+] or downvote with a [-]: ");
+                try
+                {
+                    var vote = Console.ReadLine();
+                    post.Vote(vote);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
