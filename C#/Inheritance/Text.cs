255cf779a72c0e463f1ab77a7a4af1dd1b081d73
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-07 15:36:20 -0700

    example of using inheritance

---
+++ C#/Inheritance/Text.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public int FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("Link to " + url + "added.");
+        }
+    }
+}
