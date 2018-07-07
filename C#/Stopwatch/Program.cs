7c3be7b2f1590be9ac43ec6a171cfa00e68a0626
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-06 19:47:10 -0700

    build stopwatch that can be started and stopped

---
+++ C#/Stopwatch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new Stopwatch();
+            Console.WriteLine("Welcome to the stopwatch app");
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine(watch.GetStatus() ?
+                                  "Press [Enter] to stop the timer!" :
+                                  "Press [Enter] to start the timer!");
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    if (watch.GetStatus())
+                    {
+                        watch.Stop();
+                        Console.WriteLine("Elapsed time: {0}", watch.GetTime().ToString(@"s\.ff"));
+                    }
+                    else
+                    {
+                        watch.Start();
+                        Console.WriteLine("Timer is running...");
+                    }
+                }
+                else 
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ C#/Stopwatch/Stopwatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private bool _isRunning = false;
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private TimeSpan _elapsedTime;
+
+        public Stopwatch()
+        {
+            _elapsedTime = new TimeSpan();
+        }
+
+        public TimeSpan GetTime()
+        {
+            return _elapsedTime;
+        }
+
+        public bool GetStatus()
+        {
+            return _isRunning;
+        }
+
+        public void Start()
+        {
+            if (_isRunning == false)
+            {
+                _isRunning = true;
+                _startTime = DateTime.Now;
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot start watch twice in a row!");
+            }
+        }
+
+        public void Stop()
+        {
+            if (_isRunning == true)
+            {
+                _isRunning = false;
+                _stopTime = DateTime.Now;
+                _elapsedTime += (_stopTime - _startTime);
+
+            }
+        }
+    }
+}
