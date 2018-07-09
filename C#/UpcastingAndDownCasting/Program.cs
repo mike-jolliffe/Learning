8a1ac1a650be4669805994bb4def2863aaa6016d
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-08 18:06:51 -0700

    upcasting and downcasting examples

---
+++ C#/UpcastingAndDownCasting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UpcastingAndDownCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Text text = new Text();
+            Shape shape = text;
+
+            text.Width = 200;
+            shape.Width = 100;
+
+            Console.WriteLine(text.Width);
+
+            Shape shape2 = new Text();
+            Text text2 = (Text) shape2;
+            text2.FontName = "Courier";
+        }
+    }
+}
---
+++ C#/UpcastingAndDownCasting/Text.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace UpcastingAndDownCasting
+{
+    public class Text : Shape
+    {
+        public string FontName { get; set; }
+        public int FontSize { get; set; }
+
+    }
+}
