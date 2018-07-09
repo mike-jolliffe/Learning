1784c4fa6114ec954519929d3c287b286211b1c7
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-08 17:05:51 -0700

    using : base to access info in a parent class

---
+++ C#/Constructors/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Constructors
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        //public Vehicle()
+        //{
+        //    Console.WriteLine("Vehicle being initialized...{0}", _registrationNumber);
+        //}
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+
+            Console.WriteLine("Vehicle being initialized...{0}", registrationNumber);
+        }
+    }
+}
