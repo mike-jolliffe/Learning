518562e88b472a4f69ff2de54383de5476c52da6
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-09 19:35:56 -0700

    recreate the functionality of a Stack

---
+++ C#/Stack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            try
+            {
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ C#/Stack/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+namespace Stack
+{
+    public class Stack
+    {
+        private ArrayList _list = new ArrayList();
+
+        public void Push(object obj)
+        {
+            _list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_list.Count > 0)
+            {
+                var lastIndex = _list.Count - 1;
+                var item = _list[lastIndex];
+                _list.RemoveAt(lastIndex);
+                return item; 
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("No more objects on stack.");
+            }
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
