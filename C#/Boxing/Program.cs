5f39eb79a5497e942686010d6ec8ab01a413eefc
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-09 18:51:41 -0700

    boxing and value vs. reference types

---
+++ C#/Boxing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boxing happens bc passing value type to something that needs object
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Mike");
+            list.Add(DateTime.Today);
+
+            // No boxing happens, stores int values not object references
+            var anotherList = new List<int>();
+            anotherList.Add(1);
+        }
+    }
+}
