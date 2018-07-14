f2ef0bb3c0bdedf18a1c83bcced0d9ae3a2d6810
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-13 20:16:24 -0700

    implementing a database command class that uses existing connection object and writes SQL to appropriate database

---
+++ C#/DbConnector/DbCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace DbConnector
+{
+    public class DbCommand
+    {
+        private DbConnection _connection;
+        private readonly string _command;
+
+        public DbCommand(DbConnection connection, string command)
+        {
+            _connection = connection ?? throw new ArgumentNullException(nameof(connection));
+
+            if (String.IsNullOrWhiteSpace(command))
+            {
+                throw new ArgumentException("Please provide a valid DB command for execution.");
+            }
+
+            _command = command;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Executing command: {0}", _command);
+        }
+    }
+}
--- C#/DbConnector/Program.cs
+++ C#/DbConnector/Program.cs
@@ -10,10 +10,14 @@ namespace DbConnector
             {
                 var sqlConn = new SqlConnection("SQL conn string");
                 sqlConn.Open();
+                var getEmployees = new DbCommand(sqlConn, "select * from Employees");
+                getEmployees.Execute();
                 sqlConn.Close();
 
                 var orcConn = new OracleConnection("Oracle conn string");
                 orcConn.Open();
+                var getOracleEmployees = new DbCommand(orcConn, "Some PL/SQL statement");
+                getOracleEmployees.Execute();
                 orcConn.Close();
             }
             catch (ArgumentException e)
