d8162163d2c6e99520673aaab471d7ce88c2226e
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-25 14:35:34 -0700

    build simple calculator

---
+++ C#/SimpleCalculator/Input.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace SimpleCalculator
+{
+    public class Input
+    {
+        private readonly string _inputMsg;
+
+        public Input(string inputMsg)
+        {
+            _inputMsg = inputMsg;
+        }
+
+        public string GetInput(string type)
+        {
+            Console.WriteLine(_inputMsg);
+            var input = Console.ReadLine();
+
+            if (type == "num")
+            {
+                try
+                {
+                    Int32.Parse(input);
+                    return input;
+                }
+                catch (Exception ex)
+                {
+                    if (ex is FormatException || ex is ArgumentNullException)
+                    {
+                        throw new Exception("Input must be a valid integer...");
+                    }
+
+                    throw new Exception("Something went wrong.");
+                }
+            }
+
+            if (type == "op")
+            {
+                return input.ToLower();      
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(input), "Please specify 'num' or 'op' as input type");
+        }
+    }
+}
---
+++ C#/SimpleCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Get inputs
+                var input = new Input("Number1: ");
+                var num1 = input.GetInput("num");
+
+                var input2 = new Input("Number2: ");
+                var num2 = input2.GetInput("num");
+
+                var input3 = new Input("Operation: ");
+                var op = input3.GetInput("op");
+
+                Console.WriteLine("{0} {2} {1}", num1, num2, op);
+
+                // Check inputs valid
+                var checker = new Checker(num1, num2, op);
+                if (checker.Check())
+                {
+                    checker.Execute();    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
