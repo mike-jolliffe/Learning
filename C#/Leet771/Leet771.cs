308889e57e71c369aa8516fba8a2064f6a26abee
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-07-31 13:22:23 -0700

    coding challenge. Count occurrences of certain letters in a string

---
+++ C#/Leet771/Leet771.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Leet771
+{
+    public class Leet771
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            var jewelCount = 0;
+            foreach(var jewel in J)
+            {
+                foreach(var stone in S)
+                {
+                    if(jewel == stone)
+                    {
+                        jewelCount++;
+                    }
+                }
+            }
+
+            return jewelCount;
+        }
+
+        public static void Main(string[] args)
+        {
+            var soln = new Leet771();
+            Console.WriteLine("Answer: {0} should be 3", soln.NumJewelsInStones("aA", "aAAbbbb")); 
+        }
+    }
+}
+
